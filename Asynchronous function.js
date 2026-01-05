8be8f819ba53cf75f4a2063e02bb0c00735c42a9
sobhika11 <sobhika1105@gmail.com>
2026-01-05 12:15:14 +0530

    Async function

--- Asynchronous function.js
+++ Asynchronous function.js
@@ -5,3 +5,10 @@ setTimeout(() => {
 console.log(`end`);
 
 //example of async function where the timeout runs ||ely ans doesnt stop start and end
+
+
+//promises methods
+1.promise.all()     //- it takes array of promises and returns single promise which resolves when all the promises are resolved
+2.promise.race()    //- runs promises one by one simultaeneously ,it takes array of promises and returns single promise which resolves or rejects as soon as one of the promises is resolved or rejected
+3.promise.allSettled() //- it takes array of promises and returns single promise which resolves after all the given promises are either resolved or rejected
+4.promise.any()     //- it takes array of promises and returns single promise which resolves as soon as any of the promises is resolved and rejects if all the promises are rejected -returns 1 st fulfilled promise
