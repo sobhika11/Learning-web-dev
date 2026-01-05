1017944bf5b8b30d7248e0dbd3835597d1082bc9
sobhika11 <sobhika1105@gmail.com>
2026-01-05 13:24:30 +0530

    Async function

--- Asynchronous function.js
+++ Asynchronous function.js
@@ -8,7 +8,7 @@ console.log(`end`);
 
 
 //promises methods
-1.promise.all()     //- it takes array of promises and returns single promise which resolves when all the promises are resolved
-2.promise.race()    //- runs promises one by one simultaeneously ,it takes array of promises and returns single promise which resolves or rejects as soon as one of the promises is resolved or rejected
-3.promise.allSettled() //- it takes array of promises and returns single promise which resolves after all the given promises are either resolved or rejected
-4.promise.any()     //- it takes array of promises and returns single promise which resolves as soon as any of the promises is resolved and rejects if all the promises are rejected -returns 1 st fulfilled promise
+// 1.promise.all()     //- it takes array of promises and returns single promise which resolves when all the promises are resolved
+// 2.promise.race()    //- runs promises one by one simultaeneously ,it takes array of promises and returns single promise which resolves or rejects as soon as one of the promises is resolved or rejected
+// 3.promise.allSettled() //- it takes array of promises and returns single promise which resolves after all the given promises are either resolved or rejected
+// 4.promise.any()     //- it takes array of promises and returns single promise which resolves as soon as any of the promises is resolved and rejects if all the promises are rejected -returns 1 st fulfilled promise
