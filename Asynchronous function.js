3e63b0f7a6aa5dbcd4bf6d231f26eef2a32bb75b
sobhika11 <sobhika1105@gmail.com>
2026-01-05 12:09:32 +0530

    Async function

---
+++ Asynchronous function.js
@@ -0,0 +1,7 @@
+console.log(`start`);
+setTimeout(() => {
+    console.log("inside timeout");
+}, 2000);
+console.log(`end`);
+
+//example of async function where the timeout runs ||ely ans doesnt stop start and end
