1b5f77ca824d2ab97d03769355557b81859a8634
sobhika11 <sobhika1105@gmail.com>
2026-01-05 09:56:31 +0530

    Arrays

--- Arrays.js
+++ Arrays.js
@@ -3,6 +3,10 @@ for(let i of a)
     console.log(i)
 
 //functions
-// 1.map
+// 1.map - this maps the old array elem to given condition and returns new array
 const b=a.map(i=>i+20)
 console.log(b)
+
+//2.filter - it is used to filter the array based on out conditon
+const filteredArray=a.filter(i=>i>=20)
+console.log(filteredArray)
