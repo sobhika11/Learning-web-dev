614d9c4260893c6a1cdd9ba455db44aaa721d4b3
sobhika11 <sobhika1105@gmail.com>
2026-01-05 10:19:03 +0530

    Arrays

--- Arrays.js
+++ Arrays.js
@@ -27,3 +27,11 @@ const obj={
 }
 const{name,dept}=obj
 console.log(dept)
+
+// spread operator -...
+const arr1=[1,2,3]
+const arr2=[4,5,6]          
+const arr3=[...arr1,...arr2]
+console.log(arr3)
+// it is used to copy array or object
+
