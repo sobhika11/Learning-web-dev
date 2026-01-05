3657288180d927ac3a729e8986861992aa743b1f
sobhika11 <sobhika1105@gmail.com>
2026-01-05 10:10:42 +0530

    Arrays

--- Arrays.js
+++ Arrays.js
@@ -10,3 +10,20 @@ console.log(b)
 //2.filter - it is used to filter the array based on out conditon
 const filteredArray=a.filter(i=>i>=20)
 console.log(filteredArray)
+
+// 3.find - it finds the elem which satisfies the CSSConditionRule
+// 4.reduce
+
+// destrucuting -array ,Object
+// array - Any name can be used,and []
+const nme=["sobhi","maddy","kani"]
+const[fi,se] =nme
+console.log(fi)
+
+//object- {} used and object oda key name exactly wants to be given
+const obj={
+    name:"sobhi",
+    dept :"csbs"
+}
+const{name,dept}=obj
+console.log(dept)
