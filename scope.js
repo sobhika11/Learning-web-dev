742fdf5df1ef2f184af535e9e200947d7fe8a221
sobhika11 <sobhika1105@gmail.com>
2026-01-05 09:29:44 +0530

    lexical scope

--- scope.js
+++ scope.js
@@ -1,5 +1,7 @@
 //lexical scope - A function can be called where it is written
 
+const { before } = require("node:test")
+
 function outer(){
     let msg="hello"
     function inner(){
@@ -9,3 +11,12 @@ function outer(){
 }
 outer()
 
+
+//hoisting - var variables can be used before initialized,it will be moved to top
+//  let and const doesnt work cause -tdz (temporal dead zone) so as these 2 has block scope
+//   it has tdz so cannot beforehoisted,var doesn't goes to tdz
+// tdz -{} the variable time btween {}
+
+console.log(st)
+var st="i am hoisted"
+// output will be undefined
