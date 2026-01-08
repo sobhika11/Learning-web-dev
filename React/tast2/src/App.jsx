dd3ae5651ddfc694db1f22f5fec7bf0ef036d894
sobhika11 <sobhika1105@gmail.com>
2026-01-08 13:55:42 +0530

    React

---
+++ React/tast2/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './Header.jsx'
+const App = () => {
+  return (
+    <>
+    <Header />
+    </>
+  )
+}
+
+export default App;
---
+++ React/tast2/src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+function Header(){
+  return (
+    <>
+    <header>
+        <img src="globe.png" />
+        <h1>My Journey Travel</h1>
+  </header>
+    </>
+  )
+}
+export default Header;
