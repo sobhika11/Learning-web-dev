335c324c372b2745808cbb2f8e32caedd5a8ff1b
Sobhika <sobhika1105@gmail.com>
2026-06-17 12:04:16 +0530

    List rendering

--- React/task5/src/Proptypes.jsx
+++ React/task5/src/Proptypes.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import PropTypes from "prop-types";
 const Proptypes = ({name,age}) => {
+
+    const li= ['Peter', 'Sachin', 'Kevin', 'Dhoni', 'Alisa']; 
+    const items=li.map((li,i)=>{
+        return <li key={i}>{li}</li>;
+    })
   return (
    <>
         <div>
             <h1>Hello I'm {name} </h1>
             <h2>{age}</h2>
+            <h3>{items}</h3>
         </div>
    </>
   );
